a81ce683aacad17aeaf567f1660412b6909aac61
Developenguin <matthijs@heavymetalpenguin.com>
2017-12-29 20:35:14 +0100

    No more npm package, we can build the URL ourselves

--- src/app/services/pokemon-images.service.ts
+++ src/app/services/pokemon-images.service.ts
@@ -1,20 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class PokemonImagesService {
 
    CSE_ID: string;
    API_KEY: string;
+   API_ROOT: string;
 
-  constructor() {
-     this.CSE_ID = '009690538093782806565:0gwn4hxn6mo';
-     this.API_KEY = 'AIzaSyA4SK58-GIEDNUqJ1P5KI9an7SsXg8h-UE';  // Restricted
+  constructor(private http: HttpClient) {
+    this.API_ROOT = 'https://www.googleapis.com/customsearch/v1'
+    this.CSE_ID = '009690538093782806565:0gwn4hxn6mo';
+    this.API_KEY = 'AIzaSyA4SK58-GIEDNUqJ1P5KI9an7SsXg8h-UE';  // Restricted
 
   }
 
   getImageForPokemon(pokemonName) {
 
+    const url = `${this.API_ROOT}?key=${this.API_KEY}&cx=${this.CSE_ID}&q=${pokemonName}`; //eslint-disable-line
 
+    return this.http.get(url);
 
   }
 
