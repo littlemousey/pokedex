f51d95780edaa8bbd3b8afc1a909f0b5ca4ea361
Ans de Nijs <ansdenijs@gmail.com>
2018-01-01 12:01:04 +0100

    component pokemon-list for list pokemon names added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
 import { PokemonService } from './services/pokemon.service';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonDetailComponent
+    PokemonDetailComponent,
+    PokemonListComponent
   ],
   imports: [
     BrowserModule,
